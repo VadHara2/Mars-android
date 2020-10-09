03edcf81f6634d9b63ddb295e9c8563b6511b22d
Vadym <49442821+VadHara2@users.noreply.github.com>
2020-10-09 15:40:26 +0300

    add a detail screen that displays more information for a Mars property. Also add all the usual infrastructure for adding a click listener and navigating to the new screen.

--- andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -45,6 +45,10 @@ class OverviewViewModel : ViewModel() {
     val properties: LiveData<List<MarsProperty>>
         get() = _properties
 
+    private val _navigateToSelectedProperty = MutableLiveData<MarsProperty>()
+    val navigateToSelectedProperty: LiveData<MarsProperty>
+        get() = _navigateToSelectedProperty
+
     private var viewModelJob = Job()
     private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main )
     /**
@@ -78,5 +82,12 @@ class OverviewViewModel : ViewModel() {
         viewModelJob.cancel()
     }
 
+    fun displayPropertyDetails(marsProperty: MarsProperty) {
+        _navigateToSelectedProperty.value = marsProperty
+    }
+
+    fun displayPropertyDetailsComplete() {
+        _navigateToSelectedProperty.value = null
+    }
 
 }
